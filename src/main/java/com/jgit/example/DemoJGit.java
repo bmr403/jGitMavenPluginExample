d8057018db08fa6316d2dfc56a4f8f6c140e6740
Manjunath <manjunath@bpatech.com>
2016-03-08 18:27:51 +0530

    first commit with jGit maven

---
+++ src/main/java/com/jgit/example/DemoJGit.java
@@ -0,0 +1,14 @@
+package com.jgit.example;
+
+public class DemoJGit {
+
+	public DemoJGit() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
